d33c68f0f5326070786e2048f22e8e86093eacc2
Conley Koeberle <conleykoeberle@icloud.com>
2025-07-06 11:40:14 -0500

    more commenting

--- camera.go
+++ camera.go
@@ -16,7 +16,10 @@ func (cam *ShipCamera) HandleInput() { //Takes players input and updates camera'
 	var rotationSpeed float32 = 0.05
 	var movementSpeed float32 = 0.05
 
+	//Creates a rotation matrix from our camera's current orientation to align the global forward and right vectors to our current
+	//view direction
 	rotMatrix := rl.QuaternionToMatrix(cam.Rotation)
+	//Aligns our direction vectors by multiplying the global forward and right directions
 	forward := rl.Vector3Transform(rl.NewVector3(0, 0, -1), rotMatrix)
 	right := rl.Vector3Transform(rl.NewVector3(1, 0, 0), rotMatrix)
 
@@ -59,15 +62,19 @@ func (cam *ShipCamera) HandleInput() { //Takes players input and updates camera'
 
 	//Movement functions
 	if rl.IsKeyDown(rl.KeyW) {
+		//Move forwards
 		cam.Position = rl.Vector3Add(cam.Position, rl.Vector3Scale(forward, movementSpeed))
 	}
 	if rl.IsKeyDown(rl.KeyS) {
+		//Move backwards
 		cam.Position = rl.Vector3Subtract(cam.Position, rl.Vector3Scale(forward, movementSpeed))
 	}
 	if rl.IsKeyDown(rl.KeyA) {
+		//Move left
 		cam.Position = rl.Vector3Subtract(cam.Position, rl.Vector3Scale(right, movementSpeed))
 	}
 	if rl.IsKeyDown(rl.KeyD) {
+		//Move right
 		cam.Position = rl.Vector3Add(cam.Position, rl.Vector3Scale(right, movementSpeed))
 	}
 }

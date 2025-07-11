570276030b5dfecc342622438ad08914d8272f70
Conley Koeberle <conleykoeberle@icloud.com>
2025-07-11 15:07:24 -0500

    planets can now spin around their own tilted axis

--- main.go
+++ main.go
@@ -2,19 +2,21 @@ package main
 
 import (
 	"fmt"
+	"math"
 
 	rl "github.com/gen2brain/raylib-go/raylib"
 )
 
 // 1 unit = 1km
-const EarthRadius float32 = 6371 //in KM
-const MoonRadius float32 = 1737  //in KM
-const shipLength float32 = 15    //in M
 const KMtoUnits float32 = 0.001
+const EarthRadius float32 = 6371 //in KM
+const EarthRotationSpeed float32 = float32((2 * math.Pi) / 86164.0)
+const MoonRadius float32 = 1737 //in KM
+const shipLength float32 = 1    //in KM
 
 func main() {
 	var shipCam = ShipCamera{ //Setting up custom camera
-		Position:      rl.NewVector3(0, 0, 16400),
+		Position:      rl.NewVector3(0, 0, 67311),
 		Forward:       rl.NewVector3(0, 0, 1),
 		Up:            rl.NewVector3(0, 1, 0),
 		Right:         rl.NewVector3(1, 0, 0),
@@ -41,33 +43,51 @@ func main() {
 	sidewinder := rl.LoadModel("sidewinder.obj")
 	sideTex := rl.LoadTexture("sidewindertextest.png")
 	//sidewinder.Materials.Maps.Texture = sideTex
-	fmt.Println(EarthRadius * KMtoUnits)
-	fmt.Println(shipLength * KMtoUnits)
 
 	var Earth = Planet{
-		&Sphere,
-		&SphereTex,
-		rl.NewVector3(0, 0, 0),
-		EarthRadius,
+		Model:         &Sphere,
+		Texture:       &SphereTex,
+		Position:      rl.NewVector3(0, 0, 0),
+		Radius:        EarthRadius,
+		RotationSpeed: EarthRotationSpeed,
+		AxialTilt:     23.5,
+		Axis:          rl.NewVector3(0, 1, 0),
+		Rotation:      0,
 	}
 
 	var Moon = Planet{
-		&MoonM,
-		&MoonTex,
-		rl.NewVector3(0, 0, 384400),
-		MoonRadius,
+		Model:         &MoonM,
+		Texture:       &MoonTex,
+		Position:      rl.NewVector3(0, 0, 384400),
+		Radius:        MoonRadius,
+		RotationSpeed: 0.05,
+		AxialTilt:     6.68,
+		Axis:          rl.NewVector3(0, 1, 0),
+		Rotation:      0,
 	}
 
 	Sphere.Materials.Maps.Texture = *Earth.Texture
 	MoonM.Materials.Maps.Texture = *Moon.Texture
 
+	Earth.Axis = rl.NewVector3(
+		float32(math.Sin(float64(Earth.AxialTilt*rl.Deg2rad))),
+		float32(math.Cos(float64(Earth.AxialTilt*rl.Deg2rad))),
+		0,
+	)
+	fmt.Println(Earth.Axis)
+
 	//Main game loop
 	for !rl.WindowShouldClose() {
+
 		//Updating our main camera based on the parameters of our custom one
 		rlCamera.Position = rl.NewVector3(0, 0, 0)
 		rlCamera.Target = rl.Vector3Normalize(shipCam.Forward)
 		rlCamera.Up = shipCam.Up
 		//shipCam.Position = rl.NewVector3(0, 0, 0)
+
+		deltaTime := rl.GetFrameTime()
+
+		Earth.Rotation = Earth.Rotation + Earth.RotationSpeed*deltaTime
 		//Draw loop begins
 		rl.BeginDrawing()
 		rl.ClearBackground(rl.Black)
@@ -77,13 +97,23 @@ func main() {
 		shipCam.HandleRotation() //Handle rotation
 
 		//getting scaled down positions of the earth, moon, and test sidewinder
-		relShipPos := rl.Vector3Subtract(rl.NewVector3(0, 0, 6731), shipCam.Position)
+		relShipPos := rl.Vector3Subtract(rl.NewVector3(0, 0, 67311), shipCam.Position)
 		relEarthPos := rl.Vector3Subtract(Earth.Position, shipCam.Position)
 		relMoonPos := rl.Vector3Subtract(Moon.Position, shipCam.Position)
 
 		//drawing our models at the correct scale and positions (scaled down again so that they will be within the view frustum)
-		rl.DrawModel(*Earth.Model, rl.Vector3Scale(relEarthPos, KMtoUnits), (Earth.Radius)*KMtoUnits, rl.White)
+		rl.DrawModelEx(
+			*Earth.Model,
+			rl.Vector3Scale(relEarthPos, KMtoUnits),
+			Earth.Axis,
+			Earth.Rotation*rl.Rad2deg,
+			rl.NewVector3((Earth.Radius)*KMtoUnits, (Earth.Radius)*KMtoUnits, (Earth.Radius)*KMtoUnits),
+			rl.White)
 		rl.DrawModel(*Moon.Model, rl.Vector3Scale(relMoonPos, KMtoUnits), Moon.Radius*KMtoUnits, rl.White)
+
+		//Earth Axis
+		lineEnd := rl.Vector3Scale(rl.Vector3Add(relEarthPos, rl.Vector3Scale(Earth.Axis, 10000.0)), KMtoUnits)
+		rl.DrawLine3D(rl.Vector3Scale(relEarthPos, KMtoUnits), lineEnd, rl.Red)
 
 		//draws our test sidewinder and its wireframe
 		rl.DrawModel(sidewinder, rl.Vector3Scale(relShipPos, KMtoUnits), shipLength*KMtoUnits, rl.Red)
--- planet.go
+++ planet.go
@@ -3,8 +3,12 @@ package main
 import rl "github.com/gen2brain/raylib-go/raylib"
 
 type Planet struct {
-	Model    *rl.Model
-	Texture  *rl.Texture2D
-	Position rl.Vector3
-	Radius   float32
+	Model         *rl.Model
+	Texture       *rl.Texture2D
+	Position      rl.Vector3
+	Radius        float32
+	RotationSpeed float32
+	AxialTilt     float64
+	Axis          rl.Vector3
+	Rotation      float32
 }
